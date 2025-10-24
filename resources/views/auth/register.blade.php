0c40b2e5bfbd9b7e929565dcaf57bd1dcbfee759
Idelcio <idelcioforest@gmail.com>
2025-10-24 12:08:26 -0300

    Fix: COrrige checkbos de login e register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,21 +5,24 @@
         <!-- Name -->
         <div>
             <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required
+                autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
 
         <!-- Email Address -->
         <div class="mt-4">
             <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')"
+                required autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <!-- WhatsApp Number -->
         <div class="mt-4">
             <x-input-label for="whatsapp_number" value="WhatsApp" />
-            <x-text-input id="whatsapp_number" class="block mt-1 w-full" type="text" name="whatsapp_number" :value="old('whatsapp_number')" placeholder="+5511999999999" required />
+            <x-text-input id="whatsapp_number" class="block mt-1 w-full" type="text" name="whatsapp_number"
+                :value="old('whatsapp_number')" placeholder="+5511999999999" required />
             <x-input-error :messages="$errors->get('whatsapp_number')" class="mt-2" />
         </div>
 
@@ -27,10 +30,8 @@
         <div class="mt-4">
             <x-input-label for="password" :value="__('Password')" />
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
+            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                autocomplete="new-password" />
 
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
@@ -39,9 +40,8 @@
         <div class="mt-4">
             <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
 
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
+            <x-text-input id="password_confirmation" class="block mt-1 w-full" type="password"
+                name="password_confirmation" required autocomplete="new-password" />
 
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
@@ -49,10 +49,12 @@
         <!-- Terms and Conditions -->
         <div class="mt-4">
             <label for="terms" class="inline-flex items-start">
-                <input id="terms" type="checkbox" name="terms" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" required>
+                <input id="terms" type="checkbox" name="terms"
+                    class="rounded border-gray-300 text-blue-600 shadow-sm focus:ring-blue-500" required>
                 <span class="ms-2 text-sm text-gray-600">
                     Li e aceito os
-                    <a href="{{ route('terms') }}" target="_blank" class="underline text-indigo-600 hover:text-indigo-900">
+                    <a href="{{ route('terms') }}" target="_blank"
+                        class="underline text-indigo-600 hover:text-indigo-900">
                         Termos de Uso e Política de Privacidade
                     </a>
                 </span>
@@ -61,11 +63,13 @@
         </div>
 
         <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
+            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                href="{{ route('login') }}">
                 {{ __('Already registered?') }}
             </a>
 
-            <button type="submit" class="ms-4 inline-flex items-center px-4 py-2 bg-[#25D366] border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest shadow-lg hover:shadow-xl focus:shadow-xl hover:bg-[#1EBB54] focus:bg-[#1EBB54] active:bg-[#189949] focus:outline-none focus:ring-2 focus:ring-[#25D366] focus:ring-offset-2 transition ease-in-out duration-150">
+            <button type="submit"
+                class="ms-4 inline-flex items-center px-4 py-2 bg-[#25D366] border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest shadow-lg hover:shadow-xl focus:shadow-xl hover:bg-[#1EBB54] focus:bg-[#1EBB54] active:bg-[#189949] focus:outline-none focus:ring-2 focus:ring-[#25D366] focus:ring-offset-2 transition ease-in-out duration-150">
                 {{ __('Register') }}
             </button>
         </div>
